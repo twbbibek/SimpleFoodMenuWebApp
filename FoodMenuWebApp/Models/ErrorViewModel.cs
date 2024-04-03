a83e3b7a4b751aebedd2d1832e410d9a1b96a8ef
twbbibek <bibek4929@gmail.com>
2024-04-03 23:39:33 +0545

    Add project files.

---
+++ FoodMenuWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodMenuWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
